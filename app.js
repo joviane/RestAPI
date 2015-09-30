fc323259272584092b6389c8075327981097f3ed
Marco Bruno <marco.bruno.br@gmail.com>
2015-09-29 21:34:29 -0300

    Feita uma conexao marota com o banco de dados

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ var express = require('express');
 var app = express();
 
 app.get('/aulas', function(req, res) {
-  res.send('Ae!');
+  var mysql = require('mysql');
+
+  var connection = mysql.createConnection({
+    host      : 'localhost',
+    user      : 'root',
+    password  : '',
+    database  : 'caelumweb2'
+  });
+
+  connection.query('select * from Sala', function(err, result, fields) {
+    res.send(result);
+  });
 });
 
 var server = app.listen(3000, function() {
-  console.log('Servidor rodando em: localhost:3000');
+  var host = server.address().address;
+  var port = server.address().port;
+
+  if (host == '::') {
+    host = 'localhost';
+  };
+
+  console.log('Servidor rodando em ... ' + host + ':' + port);
 });
