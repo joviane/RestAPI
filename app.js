e734faaf3d87a1d3a05433af96d5809361b6a08c
Marco Bruno <marco.bruno.br@gmail.com>
2015-09-30 20:31:37 -0300

    Finalizado de forma bem tosca (codigo) pra requisicao get do funcionalidade de Aulas da Semana

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+var formataData = function(data) {
+  var result = data.getUTCDate() + '/' + (data.getUTCMonth() + 1) + '/' + data.getUTCFullYear();
+
+  return result;
+};
+var pegarDiaDaSemana = function(data) {
+  var diaDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta'];
+
+  return diaDaSemana[data.getUTCDay()];
+};
+
 app.get('/aulas', function(req, res) {
   var mysql = require('mysql');
 
@@ -14,6 +25,21 @@ app.get('/aulas', function(req, res) {
   var query = 'select s.nome as sala, c.codigo as curso, d.dias as dia, h.nome as horario from Sala s, Curso c, Turma t, Turma_dias d, InstrutorEmAula i, Horario h where c.id = t.curso_id and t.sala_id = s.id and t.id = i.turma_id and t.id = d.turma_id and t.horario_id = h.id';
 
   connection.query(query, function(err, result, fields) {
+    result.forEach(function(element) {
+      var horario = {};
+      var data = new Date(element.dia);
+
+      element.sala = element.sala.replace('Sala ', '');
+      element.dia = formataData(data);
+
+      var horarioArray = element.horario.replace(/Manhã \(|Tarde \(|Integral \(|Noturno \(|Sabado \(|Domingo \(/, '').replace(')', '').split(' as ');
+      horario.inicio = horarioArray[0];
+      horario.fim = horarioArray[1];
+      element.horario = horario;
+
+      element.diaDaSemana = pegarDiaDaSemana(data);
+    });
+
     res.send(result);
   });
 });
